7d44994abbffd6cfef87069fb7d78a0d45871043
Mohanakrishnaa-R <mohankrish112005@gmail.com>
2025-10-04 13:10:42 +0530

    Update Project details and references

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,30 +4,30 @@ const projects = [
   {
     title: "Airport Management System",
     description: "A comprehensive SQL database for efficient management of airplanes and technicians, including detailed technician profiles and maintenance records",
-    techStack: ["SQL", "HTML", "CSS", "Django", "Python"],
+    techStack: ["MySQL", "React.js", "Flask"],
     image: "https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    githubLink: "https://github.com/yourusername/airport-management"
+    githubLink: "https://github.com/Mohanakrishnaa01/Airport-Management-System"
   },
   {
     title: "Bridge Game",
     description: "Bridge game using advanced algorithms and data structures, featuring bot play and real-time scoring and using Binomial heap, Hash Table, Binary search tree, and Fisher-Yates algorithm.",
     techStack: ["HTML", "CSS", "JS", "Node.js", "Express.js"],
     image: "https://images.unsplash.com/photo-1611996575749-79a3a250f948?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    githubLink: "https://github.com/yourusername/bridge-game"
+    githubLink: "https://github.com/Mohanakrishnaa01/BridgeGame"
   },
   {
     title: "Sleep Stage Classification Using EEG Signals",
     description: "Implemented a system to classify sleep stages using the EEG (C4-M1) channel.– Extracted features using Continuous Wavelet Transform (CWT) and applied bandpass filtering to reduce noise",
     techStack: ["Python", "MNE", "Stacking classifier", "PCA"],
     image: "https://images.unsplash.com/photo-1576091160550-2173dba999ef?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    githubLink: "https://github.com/yourusername/sleep-classification"
+    githubLink: "https://github.com/Mohanakrishnaa01/EEG-Sleep-Stage-Classification"
   },
   {
-    title: "Fraudulent Credit Card Transaction Detection",
-    description: "Developed a model using SMOTE and PCA for feature engineering. Tuned Random Forest, Decision Tree, XGBoost with GridSearchCV, enhanced performance using voting and stacking ensembles",
-    techStack: ["Python", "HP tuning", "Voting", "Stacking"],
+    title: "Image Identifier",
+    description: "Developed a face identification system using InsightFace and DBSCAN to cluster facial embeddings and detect unique individuals without predefined labels.",
+    techStack: ["Python", "React", "Django"],
     image: "https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    githubLink: "https://github.com/yourusername/fraud-detection"
+    githubLink: "https://github.com/Mohanakrishnaa01/Face-Recognition"
   }
 ];
 
